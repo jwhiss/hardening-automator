2acba8fdbb0cb1e02c836dc3b80124effa2187a1
jwhiss <joeljwhissel@gmail.com>
2025-11-29 01:43:46 -0700

    add require_binary, backup_file

--- modules/module_lib.sh
+++ modules/module_lib.sh
@@ -3,7 +3,24 @@
 # Joel Whissel
 
 log() {
-	local level="s1"
-	local msg="s2"
+	local level="$1"
+	local msg="$2"
 	echo "$(date '+%Y-%m-%d %H:%M:%S') [$level\ $msg" tee -a "$LOG_FILE"
+}
+
+require_binary() {
+	local bin="$1"
+	if ! command -v "$bin" >/dev/null 2>&1; then
+		log "ERROR" "Required binary '$bin' not found."
+		return 1
+	fi
+}
+
+backup_file() {
+	local file="$1"
+	if [ if "$file" ]; then
+		local backup="${file}.bak.$(date +%s)"
+		cp "$file" "$backup"
+		log "INFO" "Backup created: $backup"
+	fi
 }

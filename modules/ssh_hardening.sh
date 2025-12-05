f8f074861ff57fa4d562b591d35aae108962d96e
jwhiss <JoelJWhissel@gmail.com>
2025-12-05 16:29:38 -0700

    add checks for ssh protocols and cyphers

--- modules/ssh_hardening.sh
+++ modules/ssh_hardening.sh
@@ -13,6 +13,11 @@ backup_file /etc/ssh/sshd_config
 
 update_or_append /etc/ssh/sshd_config "PasswordAuthentication" "no"
 update_or_append /etc/ssh/sshd_config "PubkeyAuthentication" "yes"
+update_or_append /etc/ssh/sshd_config "PermitRootLogin" "no"
+update_or_append /etc/ssh/sshd_config "Protocol" "2"
+update_or_append /etc/sshd_config "KexAlgorithms" "curve25519-sha256,curve25519-sha256@libssh.org"
+update_or_append /etc/ssh/sshd_config "Ciphers" "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com"
+update_or_append /etc/ssh/sshd_config "MACs" "hmac-sha2-512,hmac-sha2-256"
 
 if systemctl list-unit-files | grep -q "^ssh.service"; then
     systemctl restart ssh

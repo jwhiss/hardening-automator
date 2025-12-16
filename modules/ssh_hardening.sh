9ceca5d168ad16667e9337252416bb9ce20d69bb
jwhiss <JoelJWhissel@gmail.com>
2025-12-15 19:47:48 -0700

    enforce login rate limiting

--- modules/ssh_hardening.sh
+++ modules/ssh_hardening.sh
@@ -18,6 +18,8 @@ update_or_append /etc/ssh/sshd_config "Protocol" "2"
 update_or_append /etc/sshd_config "KexAlgorithms" "curve25519-sha256,curve25519-sha256@libssh.org"
 update_or_append /etc/ssh/sshd_config "Ciphers" "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com"
 update_or_append /etc/ssh/sshd_config "MACs" "hmac-sha2-512,hmac-sha2-256"
+update_or_append /etc/ssh/sshd_config "MaxAuthTries" "3"
+update_or_append /etc/ssh/sshd_config "MaxSessions" "2"
 
 if systemctl list-unit-files | grep -q "^ssh.service"; then
     systemctl restart ssh
